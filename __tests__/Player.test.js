f929c8c03f8da7065075afbb944d18d3afb757d9
nHur2004 <nathan.hur04@gmail.com>
2022-08-30 17:25:02 -0600

    added player getStats() and getInventory()

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -15,3 +15,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Among Us');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from palyer or returns false', () => {
+    const player = new Player("Among Us");
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
