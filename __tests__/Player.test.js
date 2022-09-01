bb43e35b47d7152927b6934550e97159ed8b6e77
nHur2004 <nathan.hur04@gmail.com>
2022-09-01 15:12:03 -0600

    added Player object Potion methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -63,3 +63,30 @@ test("subtracts from player's health", () => {
 
     expect(player.health).toBe(0);
 });
+
+test("gets player's attack value", () => {
+    const player = new Player("Among Us");
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("adds a potion to the inventory", () => {
+    const player = new Player("Among Us");
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThanOrEqual(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player("among us");
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
