c35ff4affb57bf63229d5f79632e0fda5712057a
nHur2004 <nathan.hur04@gmail.com>
2022-09-01 14:53:05 -0600

    added Player object health mthods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -34,3 +34,32 @@ test('gets inventory from palyer or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () => {
+    const player = new Player("Among Us");
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player("Among Us");
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player("Among Us");
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
